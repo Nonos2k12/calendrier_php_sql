42b3d3d7a2a8a8f5cfbdea868a5395d11a0e016b
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-10-20 16:56:30 +0200

    Starting a new PHP/SQL project

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+
+   <title>Mon calendrier</title>
+</head>
+<body>
+   
+   <nav class="navbar navbar-dark bg-primary mb-3">
+      <a href="/index.php" class="navbar-brand">Mon calendrier</a>
+   </nav>
+
+   <h1>Mars 2018</h1>
+
+</body>
+</html>
